c9f3331deadf03511c2621f399aa3d70e7fa7705
ksnyder1986 <54869316+ksnyder1986@users.noreply.github.com>
2019-09-10 20:17:46 -0400

    Update collect_game_ids.py

--- collect_game_ids.py
+++ collect_game_ids.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep  4 08:12:29 2019
-
-@author: Kris
-"""
 import re
 import csv
 import time
@@ -13,14 +7,11 @@ from bs4 import BeautifulSoup
 
 
 base = 'http://www.boardgamegeek.com/browse/boardgame/page/{}'
-#with open('ids.txt') as f:
-#    ids = [line.strip() for line in f.readlines()]
 f = open('ids.csv', 'w')
 writer = csv.writer(f)
 writer.writerow(('id','1'))
 
-#for i in range(0, len(usernames), split):
- #   url = base.format(','.join(usernames[i:i+split]))
+
 for i in range(1095):
    url = base.format(''.join(str(i+1))) 
    print('Requesting {}'.format(url))
