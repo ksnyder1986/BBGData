170ca155340309c799efc132edec3618e48af201
ksnyder1986 <54869316+ksnyder1986@users.noreply.github.com>
2022-02-14 08:24:33 -0500

    Update collect_game_ids.py

--- collect_game_ids.py
+++ collect_game_ids.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  4 08:12:29 2019
+
+@author: Kris
+"""
 import re
 import csv
 import time
@@ -6,23 +12,34 @@ import requests
 from bs4 import BeautifulSoup
 
 
-base = 'https://www.legis.state.pa.us/cfdocs/billinfo/bill_history.cfm?syear=2019&sind=0&body=H&type=B&bn={}'
-f = open('pers.csv', 'w')
+base = 'http://www.boardgamegeek.com/browse/boardgame/page/{}'
+#with open('ids.txt') as f:
+#    ids = [line.strip() for line in f.readlines()]
+url = base.format(''.join(str(1))) 
+print('Requesting {}'.format(url))
+req = requests.get(url)
+soup = BeautifulSoup(req.content, 'lxml')
+last_page = soup.find("a",title="last page").text
+last_page = int(last_page.replace('[','').replace(']',''))
+
+f = open('ids.csv', 'w')
 writer = csv.writer(f)
 writer.writerow(('id','1'))
-i = 462
 
-for i in range(1):
+#for i in range(0, len(usernames), split):
+ #   url = base.format(','.join(usernames[i:i+split]))
+for i in range(last_page):
    url = base.format(''.join(str(i+1))) 
    print('Requesting {}'.format(url))
    req = requests.get(url)
-   soup = BeautifulSoup(req.content, 'html.parser')
-   rows = soup.find_all("div", {"class":"BillInfo-Section-Data"})[0]
-   a = str(rows).replace('and',',')
-   a = str(rows).split(',')
-   a
-   rows
-
-   time.sleep(5)
+   soup = BeautifulSoup(req.content, 'lxml')
+   rows = soup.select('tr#row_')
+   for row in rows:
+            a = row.find_all('a', href=re.compile('^/boardgame'))
+            for r in a:                
+                if r.text and r.text != 'Shop':
+                    g = r.get('href').split('/')[2]
+                    writer.writerow((g,''))
+   time.sleep(1)
 
 f.close()
